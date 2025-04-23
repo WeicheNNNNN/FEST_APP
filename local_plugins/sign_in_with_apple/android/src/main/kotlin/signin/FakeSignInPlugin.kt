7f9c1fe56756c19f3995d420fd288063fff8f5c0
WeicheNNNNN <weihan4493@gmail.com>
2025-04-23 18:49:36 +0800

    Add fake Kotlin plugin class for sign_in_with_apple

---
+++ local_plugins/sign_in_with_apple/android/src/main/kotlin/signin/FakeSignInPlugin.kt
@@ -0,0 +1,12 @@
+package fake.signin
+
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FakeSignInPlugin {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            // Do nothing
+        }
+    }
+}
